873b3e361dd4f5d36fd3d35701e029ab66b52824
liumuzi <liumuzi1215@gmail.com>
2018-03-17 23:30:16 +0800

    1.0.3
    Sign up

--- routes/personalSec/signup.js
+++ routes/personalSec/signup.js
@@ -1,33 +1,46 @@
 var express = require('express');
 var router = express.Router();
-var app = express();
 
 /* GET sign up page. */
 router.get('/', function(req, res, next) {
     res.render('personalSec/signup', {
         title: 'Sign Up',
-        name: 'Daily Cate'
+        name: 'Daily Cate',
+        errors: undefined
     });
 });
 
 router.post('/', function (req,res,next) {
-    console.log('Hey');
-    console.log(req.body.name);
+    //store in database
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+    var description = req.body.description;
+
+    //only for validation
+    var password2 = req.body.password2;
+
+    //Form validator
+    req.checkBody('name', 'Name field is required').notEmpty();
+    req.checkBody('email', 'Email field is required').notEmpty();
+    req.checkBody('email', 'Email is invalid').isEmail();
+    req.checkBody('username', 'Username field is required').notEmpty();
+    req.checkBody('password', 'Password field is required').notEmpty();
+    req.checkBody('password2', 'Password do not match').equals(req.body.password);
+
+
+    //Check errors
+    var errors = req.validationErrors();
+
+    if (errors){
+        res.render('personalSec/signup', {
+            title: 'Sign Up',
+            name: 'Daily Cate',
+            errors: errors
+        });
+    }else{
+        console.log('no errors')
+    }
 });
-
-
-// router.get('/process_get',function(req,res){
-//     var response = {
-//         "username":req.body.username,
-//         "email":req.query.email,
-//         "password":req.query.password,
-//         // "description":req.query.desc
-//     };
-//
-//     console.log(response);
-//
-//     res.end(JSON.stringify(response));
-//
-// })
 
 module.exports = router;
