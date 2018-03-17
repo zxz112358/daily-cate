797f9eedc7c7432d32cf313368a0201f058d5e2d
liumuzi <liumuzi1215@gmail.com>
2018-03-17 20:23:13 +0800

    v1.0.1 first draft by muzi

--- routes/personalSec/signup.js
+++ routes/personalSec/signup.js
@@ -9,4 +9,22 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.post('/', function (req,res,next) {
+    console.log(req.body.email);
+});
+
+// router.get('/process_get',function(req,res){
+//     var response = {
+//         "username":req.body.username,
+//         "email":req.query.email,
+//         "password":req.query.password,
+//         // "description":req.query.desc
+//     };
+//
+//     console.log(response);
+//
+//     res.end(JSON.stringify(response));
+//
+// })
+
 module.exports = router;
