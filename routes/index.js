c17e11819a7bd75d0e9e94f176a0d1da8623e95d
zxz112358 <36069381+zxz112358@users.noreply.github.com>
2018-03-18 16:28:49 +0800

    home page modified a little bit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Home',
+    title: 'Welcome to Daily Cate!',
     name:'Daily Cate'
   });
 });
