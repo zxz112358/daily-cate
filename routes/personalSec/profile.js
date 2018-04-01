b71c2a40cf02171ecd0a275774d47d956ae1bf6a
970227natalie <abcdefg19976@gmail.com>
2018-04-01 21:36:19 +0800

    dynamic pic show

--- routes/personalSec/profile.js
+++ routes/personalSec/profile.js
@@ -5,22 +5,42 @@ var router = express.Router();
 
 var test = require('../test');
 var connection = test.connection;
+var fs=require("fs");
 
 /* GET user profile page. */
 router.get('/', authenticationMiddleware(), function(req, res, next) {
 
     test.select_client_article(req.user.username,function(result1,result2){
+        var string1=[];
+        var string2=[];
+        var string3=[];
 
+        for(var i=0;i<result1;i++){
+            string1.push(result2[i].picturestart+result2[i].pictureno-1);
+            string1[i]='../exhibitionSec/pictures/'+string1[i];
+        }
+        for(var i=0;i<result1;i++){
+            string2.push(result2[i].parastart+result2[i].parano-1);
+            string2[i]='../exhibitionSec/texts/'+string2[i];
+            var data=fs.readFileSync(string2[i]);
+            string3.push(data.toString());
+        }
+
+        console.log(string3);
             res.render('personalSec/profile', {
                 title: 'Profile',
                 name: 'Daily Cate',
                 user: req.user,
                 imgpath: '../profileimgs/' + req.user.username,
                 articleno:result1,
-                result: result2
+                result: result2,
+                coverpic:string1,
+                text:string3
+
             });
-            console.log(result2.picturestart + result2.pictureno - 1);
         }
+
+
     );
 
 });
